added cluster hamiltonian Hci for each fock space to remove the tdm rotations to give different solns in different runs. CAS bug was not there, it was the tdm rotation issues. so adding a hshift parameter in bc_cipsi tucker which should fix the issue for now. recommended value : 1e-6-9

src/Cluster.py
@@ -52,6 +52,7 @@ class Cluster(object):
         self.ops    = {}
         
         self.energies = {}                  # Diagonal of local operators
+        self.Hci = {}
 
     def __len__(self):
         return len(self.orb_list) 
@@ -95,9 +96,10 @@ class Cluster(object):
                 ci.algorithm = "direct"
                 ci.init(H,na,nb,max_roots)
                 print(ci)
-                ci.run()
+                Hci = ci.run()
                 #self.basis[(na,nb)] = np.eye(ci.results_v.shape[0])
                 self.basis[(na,nb)] = ci.results_v
+                self.Hci[(na,nb)] = Hci
                 #print(ci.results_v)
 
 
@@ -127,6 +129,7 @@ class Cluster(object):
         """
         for fspace,mat in U.items():
             self.basis[fspace] = self.basis[fspace] @ mat
+            self.Hci[fspace] = self.basis[fspace].T @ self.Hci[fspace] @ self.basis[fspace]
         #print(" Build all operators:")
         #self.build_op_matrices()
         for op,fspace_deltas in self.ops.items():

src/bc_cipsi.py
@@ -15,7 +15,7 @@ from tools import *
 
 def bc_cipsi_tucker(ci_vector, clustered_ham, 
         thresh_cipsi=1e-4, thresh_ci_clip=1e-5, thresh_cipsi_conv=1e-8, max_cipsi_iter=30, 
-        thresh_tucker_conv = 1e-6, max_tucker_iter=20, tucker_state_clip=None):
+        thresh_tucker_conv = 1e-6, max_tucker_iter=20, tucker_state_clip=None,hshift=1e-8):
     """
     Run iterations of BR-CIPSI to make the tucker decomposition self-consistent
     """
@@ -50,11 +50,24 @@ def bc_cipsi_tucker(ci_vector, clustered_ham,
             norm = 0
             rotations = {}
             for fspace,rdm in rdms.items():
+                
                 print(" Diagonalize RDM for Cluster %2i in Fock space:"%ci.idx, fspace,flush=True)
                 n,U = np.linalg.eigh(rdm)
                 idx = n.argsort()[::-1]
                 n = n[idx]
                 U = U[:,idx]
+
+                if hshift != None:
+                    print(fspace)
+                    Hci = ci.Hci[fspace]
+                    print(Hci)
+                    print(rdm)
+                    n,U = np.linalg.eigh(rdm + hshift*Hci)
+                    n = np.diag(U.T @ rdm @ U)
+                    idx = n.argsort()[::-1]
+                    n = n[idx]
+                    U = U[:,idx]
+                
                 norm += sum(n)
                 for ni_idx,ni in enumerate(n):
                     if abs(ni) > 1e-12: