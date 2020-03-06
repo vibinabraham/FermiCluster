dfb562fbd04d80c418c0a371efc0d6f79ad948e3
Nick Mayhall <nmayhall@vt.edu>
2020-03-06 15:18:56 -0800

    fixed a couple bugs

--- src/ClusteredState.py
+++ src/ClusteredState.py
@@ -348,8 +348,6 @@ class ClusteredState(OrderedDict):
         Normalize state
         """
         norm = self.norm()
-        for fockspace,config,coeff in self:
-            self[fockspace][config] = coeff/norm
         for fockspace,configs in self.items():
             for config,coeff in configs.items():
                 self[fockspace][config] = coeff/norm
