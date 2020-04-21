ca50ded6e488acb9de09c6da6887dbcf9dcdd5b3
Nick Mayhall <nmayhall@vt.edu>
2020-04-21 11:21:17 -0400

    normalized error measure in pt2 correction

--- src/bc_cipsi.py
+++ src/bc_cipsi.py
@@ -407,7 +407,7 @@ def bc_cipsi(ci_vector, clustered_ham,
         # Compute the energy of the zeroth-order energy using matvec. 
         # This gives us some indication how accurate the approximations 
         # (asci_vector, and thresh_search) are
-        e0_curr = ci_vector.dot(pt_vector) 
+        e0_curr = ci_vector.dot(pt_vector)/asci_vector.dot(asci_vector) 
         print(" Zeroth-order energy: %12.8f Error in E0: %12.8f" %(e0_curr, e0_curr - e0)) 
 
         if profile:
